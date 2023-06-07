2c4b3cad7e18aa2f87f238a564e04d5fa5c83dbe
simon hu <simon.hu.1999@gmail.com>
2023-06-07 17:15:57 +0200

    Sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: "pages#home"
-  
-  Rails.application.routes.draw do
-    resources :events, only: [:index, :show]
-    # other routes...
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  resources :events, only: [:index, :show]
 end
