5153954a5a280aaf6f4243ea1f00fc618a2af9b9
Harris Grant <harris@shoptreen.com>
2023-06-12 12:00:33 +0200

    added autocomplete search functionality to the event create form

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AddressAutocompleteController from "./address_autocomplete_controller"
+application.register("address-autocomplete", AddressAutocompleteController)
+
 import EventFormController from "./event_form_controller"
 application.register("event-form", EventFormController)
 
