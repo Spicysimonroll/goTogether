c8b004a4887eccb60995c8e878fc5c8e2a360326
simon hu <simon.hu.1999@gmail.com>
2023-06-13 17:02:45 +0200

    comment section toggle

--- app/javascript/controllers/event_showpage_controller.js
+++ app/javascript/controllers/event_showpage_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="event-showpage"
 export default class extends Controller {
-  static targets = ["location", "comment", "form"]
+  static targets = ["location", "comment_section", "form", "comment"]
 
   connect() {
     console.log("event showpage controller connected!");
@@ -28,7 +28,7 @@ export default class extends Controller {
       .then(response => response.json())
       .then((data) => {
         if (data.inserted_item) {
-          this.commentTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+          this.comment_sectionTarget.insertAdjacentHTML("beforeend", data.inserted_item)
         }
         this.formTarget.outerHTML = data.form
         window.scrollTo(0, document.body.scrollHeight);
@@ -36,12 +36,15 @@ export default class extends Controller {
   }
 
   toggleComments(event) {
-    // console.log(this.commentTarget);
-    // console.log(event.currentTarget.innerHTML);
     if (event.currentTarget.innerHTML === 'Comments <i class="fa-solid fa-caret-down"></i>') {
       event.currentTarget.innerHTML = 'Comments <i class="fa-solid fa-caret-up"></i>'
     } else if (event.currentTarget.innerHTML === 'Comments <i class="fa-solid fa-caret-up"></i>') {
       event.currentTarget.innerHTML = 'Comments <i class="fa-solid fa-caret-down"></i>'
     }
+    // console.log(this.commentTargets);
+    this.commentTargets.forEach( (target) => {
+      target.classList.toggle("d-none")
+      // console.log(target);
+    })
   }
 }
--- app/views/comments/create.json.jbuilder
+++ app/views/comments/create.json.jbuilder
@@ -1,6 +1,6 @@
 if @comment.persisted?
   json.form render(partial: "comments/form", formats: :html, locals: { event: @event, comment: Comment.new })
-  json.inserted_item render(partial: "events/comment", formats: :html, locals: { comment: @comment })
+  json.inserted_item render(partial: "events/comment", formats: :html, locals: { comment: @comment, count: @event.comments.count, index: @event.comments.count })
 else
   json.form render(partial: "comments/form", formats: :html, locals: { event: @event, comment: @comment })
 end
