712bb1a88612fc06764666533a6db3e6eef12a28
Harris Grant <harris@shoptreen.com>
2023-06-13 12:04:32 +0200

    added going and not_going icons to invites index, which will set booking status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  enum status: [:going, :looking_for_a_buddy]
+  enum status: [:going, :looking_for_a_buddy, :not_going]
+  after_initialize :init
+
+  def init
+    self.status = :not_going
+  end
 end
