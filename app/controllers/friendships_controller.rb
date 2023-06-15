0fa6a91c293285059cb5cd5671623cdb42ad4041
marien irzy <marienirzyk@gmail.com>
2023-06-15 11:20:34 +0200

    friend search feature done on first_name, last_name, username with matching any string of characters

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,19 @@
 class FriendshipsController < ApplicationController
   def index
     @friendships = policy_scope(Friendship)
+
+    if params[:query].present?
+      query = "%#{params[:query]}%"
+
+      @friendships = @friendships.joins('INNER JOIN profiles AS friend_profiles ON friendships.friend_id = friend_profiles.user_id')
+                                  .where("friend_profiles.first_name ILIKE :query OR friend_profiles.last_name ILIKE :query OR friend_profiles.username ILIKE :query", query: query)
+    end
   end
+
+  private
+
+  def event_params
+    params.require(:profiles).permit(:first_name, :last_name, :username)
+  end
+
 end
